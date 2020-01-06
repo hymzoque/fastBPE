0e2fc90e2a2120c14ec4f67000ec737821661ea5
Hu <huta@a483e74650ff.ant.amazon.com>
2020-01-06 14:11:50 +0800

    update

--- fastBPE/fastBPE.hpp
+++ fastBPE/fastBPE.hpp
@@ -649,7 +649,7 @@ string process_bpe(vector<string> &subwords,
 //  }
   // concat subWords
   string result = "";
-  result += 0x2581;
+  result += (char)0x2581;
   for (auto x : subwords) {
     result = result + x + kTokenDelim + " ";
   }
